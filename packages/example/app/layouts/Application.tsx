d347418648847a8268861a5a239b89e032343711
genfuru011 <hiro_genfuru0119@icloud.com>
2025-12-16 00:32:18 +0900

    fix(example): update import map to use hyperscript jsx-runtime

--- packages/example/app/layouts/Application.tsx
+++ packages/example/app/layouts/Application.tsx
@@ -11,7 +11,7 @@ export default function Application(props: { children: JSX.Element }) {
           "imports": {
             "solid-js": "https://esm.sh/solid-js@1.8.16?target=es2022",
             "solid-js/web": "https://esm.sh/solid-js@1.8.16/web?target=es2022",
-            "solid-js/jsx-runtime": "https://esm.sh/solid-js@1.8.16/jsx-runtime?target=es2022"
+            "solid-js/jsx-runtime": "https://esm.sh/solid-js@1.8.16/h/jsx-runtime?target=es2022"
           }
         }
         `}</script>
